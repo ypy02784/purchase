f2333f34926cc1ddf84713af3a9465b089f31d68
ypy02784 <ypy02784@163.com>
2018-11-11 22:18:37 +0800

    test

--- JDpurchase.py
+++ JDpurchase.py
@@ -9,17 +9,17 @@ driver = webdriver.Chrome(executable_path="/Users/ypy/Downloads/chromedriver")
 def login(uname, pwd):
     driver.get("http://www.jd.com")
     driver.find_element_by_link_text("你好，请登录").click()
-
+    time.sleep(1)
     driver.find_element_by_link_text("账户登录").click()
     driver.find_element_by_name("loginname").send_keys(uname)
-    time.sleep(3)
+    
     driver.find_element_by_name("nloginpwd").send_keys(pwd)
-    time.sleep(3)
+    
     driver.find_element_by_id("loginsubmit").click()
+    time.sleep(10)#此处留十秒用于人操作进行滑块验证
+    driver.get("https://cart.jd.com/cart.action")
 
-    # driver.get("https://cart.jd.com/cart.action")
-
-    # driver.find_element_by_link_text("去结算").click()
+    driver.find_element_by_link_text("去结算").click()
     # now = datetime.datetime.now()
     # print('login success:',now.strftime('%Y-%m-%d %H:%M:%S'))
 
@@ -40,4 +40,4 @@ def buy_on_time(buytime):
 
 # entrance
 login('ypy02784@163.com', 'ypy19840525')
-# buy_on_time('2017-01-01 14:00:01')
+buy_on_time('2018-11-11 22:10:01')
